444b1b980edf92ea64fb5eedcd20708c9f9249d6
Shonah <69339521+shonah32@users.noreply.github.com>
2022-09-17 12:06:57 +0700

    Create script.js
    First complete version of script.js, with all the functions intended for the TTHome.html file.

---
+++ script.js
@@ -0,0 +1,67 @@
+const moreTextBtn = document.querySelector('.moreTextBtn');
+const text = document.querySelector('.text');
+moreTextBtn.addEventListener('click',(e)=>{
+    text.classList.toggle('showMore')
+})
+
+var iconTracker = "plus";
+var iconTracker2 = "plus";
+var iconTracker3 = "plus";
+
+function changeImage(){
+    var icon = document.getElementById('plusImage');
+   if(iconTracker == "plus"){
+    icon.src = "orangeMinus.png"; 
+    iconTracker = "minus"; 
+   }
+   else{
+    icon.src = "orangePlus2.png";
+    iconTracker = "plus"; 
+   }
+
+}
+
+function changeImage2(){
+    var icon = document.getElementById('plusImage2');
+   if(iconTracker2 == "plus"){
+    icon.src = "orangeMinus.png"; 
+    iconTracker2 = "minus"; 
+   }
+   else{
+    icon.src = "orangePlus2.png";
+    iconTracker2 = "plus"; 
+   }
+
+}
+
+function changeImage3(){
+    var icon = document.getElementById('plusImage3');
+   if(iconTracker3 == "plus"){
+    icon.src = "orangeMinus.png"; 
+    iconTracker3 = "minus"; 
+   }
+   else{
+    icon.src = "orangePlus2.png";
+    iconTracker3 = "plus"; 
+   }
+
+}
+
+function showImage() {
+    const policy = document.getElementById("policyImage");
+    if (policy.style.display === "block") {
+        policy.style.display = "none";
+    } else {
+        policy.style.display = "block";
+    }
+  }
+
+
+  function showDisclaimer() {
+    var disclaimer = document.getElementById("disclaimer");
+    if (disclaimer.style.display === "block") {
+        disclaimer.style.display = "none";
+    } else {
+        disclaimer.style.display = "block";
+    }
+  }
